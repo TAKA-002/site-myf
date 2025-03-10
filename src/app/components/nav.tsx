31f78b1fd2e24ebe862680fd600d22fc9cd0c70e
miura takayuki <6015abc@gmail.com>
2025-03-11 05:27:37 +0900

    ナビのフォーマットを実施

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -2,34 +2,29 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 interface NavProps {
-  navData: {
-    link: string;
-    iconCls: string;
-    text: string;
-  }[];
+	navData: {
+		link: string;
+		iconCls: string;
+		text: string;
+	}[];
 }
 
 export default function Nav({ navData }: NavProps) {
-  const pathname = usePathname();
+	const pathname = usePathname();
 
-  return (
-    <nav>
-      <ul>
-        {navData.map((n, index) => {
-          return (
-            <li key={index}>
-              <Link
-                href={n.link}
-                className={
-                  pathname === n.link ? `active ${n.iconCls}` : n.iconCls
-                }
-              >
-                {n.text}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-    </nav>
-  );
+	return (
+		<nav>
+			<ul>
+				{navData.map((n, index) => {
+					return (
+						<li key={index}>
+							<Link href={n.link} className={pathname === n.link ? `active ${n.iconCls}` : n.iconCls}>
+								{n.text}
+							</Link>
+						</li>
+					);
+				})}
+			</ul>
+		</nav>
+	);
 }
