d28e63eeeb4ee153552ed3d131b593b93cf6565c
miura takayuki <6015abc@gmail.com>
2025-03-12 23:48:51 +0900

    lucide-iconsでLangBtnのアイコンを追加

--- src/app/components/ui/buttons/LanguageBtn.tsx
+++ src/app/components/ui/buttons/LanguageBtn.tsx
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 import Link from "next/link";
+import { Earth } from "lucide-react";
 
 const Wrapper = styled.ul`
 	width: 120px;
 	height: 30px;
 	overflow: hidden;
+	border-radius: 9999px;
 
 	& li {
 		width: 100%;
@@ -12,6 +14,7 @@ const Wrapper = styled.ul`
 		font-size: 10px;
 		color: ${(props) => props.theme.colors.txtLangBtn};
 		background-color: ${(props) => props.theme.colors.bgLangBtn};
+		font-weight: bold;
 
 		&:not(:first-child) {
 			opacity: 0;
@@ -21,6 +24,9 @@ const Wrapper = styled.ul`
 		&:first-child {
 			& a {
 				pointer-events: none;
+				display: flex;
+				justify-content: space-between;
+				align-items: center;
 			}
 		}
 
@@ -28,7 +34,7 @@ const Wrapper = styled.ul`
 			display: grid;
 			align-items: center;
 			height: 100%;
-			padding-left: 8px;
+			padding: 0 8px;
 		}
 	}
 `;
@@ -37,7 +43,10 @@ export default function LanguageButton() {
 	return (
 		<Wrapper>
 			<li>
-				<a href="#">Language</a>
+				<a href="#">
+					Language
+					<Earth />
+				</a>
 			</li>
 			<li>
 				<Link href="">Japanese</Link>
