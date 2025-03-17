4aa365005ec68ff4f637d0c21f108a675d2bd307
miura takayuki <6015abc@gmail.com>
2025-03-18 03:28:10 +0900

    styleをCSS Modulesで分離

--- src/app/components/ui/buttons/LanguageBtnWrapper.tsx
+++ src/app/components/ui/buttons/LanguageBtnWrapper.tsx
@@ -1,32 +1,10 @@
 import { ReactNode } from "react";
-import styled from "styled-components";
+import styles from "./LanguageBtnWrapper.module.scss";
 
 interface LangBtnWrapperProps {
 	children: ReactNode;
 }
 
-const LangBtnWrapper = styled.div`
-	/* 579pxまで表示。580pxから非表示。 */
-	body > & {
-		position: fixed;
-		z-index: 20;
-		right: 0;
-		bottom: 120px;
-		display: block;
-
-		@media screen and (min-width: ${(props) => props.theme.breakpoint.LangBtn}) {
-			display: none;
-		}
-	}
-
-	/* 579pxまで非表示。580pxから表示。 */
-	display: none;
-
-	@media screen and (min-width: ${(props) => props.theme.breakpoint.LangBtn}) {
-		display: block;
-	}
-`;
-
 export default function LanguageBtnWrapper({ children }: LangBtnWrapperProps) {
-	return <LangBtnWrapper>{children}</LangBtnWrapper>;
+	return <div className={styles.div}>{children}</div>;
 }
