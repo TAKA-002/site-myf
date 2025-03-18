a17e2f88b5b73a3f49803e9dfa47a42cb17e4c30
miura takayuki <6015abc@gmail.com>
2025-03-18 19:53:16 +0900

    styleをCSS Modulesで分離

--- src/app/components/ui/layout/Header.tsx
+++ src/app/components/ui/layout/Header.tsx
@@ -1,73 +1,37 @@
-"use client";
-import styled from "styled-components";
 import Image from "next/image";
 import Link from "next/link";
 import { MenuBtn, LangBtn } from "../buttons";
 import { useLang } from "../../../context/LanguageContext";
+import styles from "./Header.module.scss";
 
-const Header = styled.header`
-	background-color: ${(props) => props.theme.colors.bgHeader};
-	position: relative;
-	z-index: 20;
-`;
-
-const Wrapper = styled.div`
-	display: flex;
-	flex-direction: row;
-	justify-content: space-between;
-	align-items: center;
-	gap: 16px;
-	max-width: 960px;
-	margin-inline: auto;
-	padding: 4px 8px;
-`;
-
-const BtnWrapper = styled.div`
-	display: flex;
-	flex-direction: row;
-	gap: 12px;
-`;
-
-const StyledLink = styled(Link)`
-	max-width: 365px;
-	width: 100%;
-	height: auto;
-	aspect-ratio: 365 / 63;
-
-	& img {
-		width: 100%;
-		height: 100%;
-		object-fit: cover;
-	}
-`;
+const headerLinks = {
+	ja: {
+		href: "/",
+		img: "/images/common/logo.png",
+		alt: "ガーナの未来ある子供達に笑顔と教育を！マンフィーと青年基金NGO",
+	},
+	en: {
+		href: "/en",
+		img: "/images/common/logo_en.png",
+		alt: "Monphy and Youth Foundation NGO",
+	},
+};
 
 export default function MainHeader() {
 	const { isEnPage } = useLang();
-	const headerLinks = {
-		ja: {
-			href: "/",
-			img: "/images/common/logo.png",
-			alt: "ガーナの未来ある子供達に笑顔と教育を！マンフィーと青年基金NGO",
-		},
-		en: {
-			href: "/en",
-			img: "/images/common/logo_en.png",
-			alt: "Monphy and Youth Foundation NGO",
-		},
-	};
 
 	return (
-		<Header>
-			<Wrapper>
-				<StyledLink href={isEnPage ? headerLinks.en.href : headerLinks.ja.href}>
+		<header className={styles.header}>
+			<div className={styles.wrapper}>
+				<Link className={styles.link} href={isEnPage ? headerLinks.en.href : headerLinks.ja.href}>
 					<Image src={isEnPage ? headerLinks.en.img : headerLinks.ja.img} alt={isEnPage ? headerLinks.en.alt : headerLinks.ja.alt} width={365} height={63} />
-				</StyledLink>
+				</Link>
 
-				<BtnWrapper>
+				<div className={styles.btnWrapper}>
 					<LangBtn />
 					<MenuBtn />
-				</BtnWrapper>
-			</Wrapper>
-		</Header>
+				</div>
+			</div>
+		</header>
 	);
 }
