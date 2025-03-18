8cc72d1a069558285fbf2001a63e9aba40e8d9d2
miura takayuki <6015abc@gmail.com>
2025-03-18 20:38:18 +0900

    styleをCSS Modulesで分離

--- src/app/components/ui/navigation/NavList.tsx
+++ src/app/components/ui/navigation/NavList.tsx
@@ -1,99 +1,26 @@
-import styled from "styled-components";
 import Link from "next/link";
 import { nav } from "../../../constants/navigations";
+import { useMenu } from "../../../context/MenuContext";
 import { useLang } from "../../../context/LanguageContext";
-
-const NavUl = styled.ul`
-	display: flex;
-	flex-direction: column;
-	gap: min(6.4vw, 24px);
-
-	& li {
-		opacity: 0;
-		transform: translateY(20px);
-		transition:
-			opacity 0.3s ease,
-			transform 0.3s ease;
-
-		&:nth-child(1) {
-			transition-delay: 0.2s;
-		}
-		&:nth-child(2) {
-			transition-delay: 0.3s;
-		}
-		&:nth-child(3) {
-			transition-delay: 0.4s;
-		}
-		&:nth-child(4) {
-			transition-delay: 0.5s;
-		}
-		&:nth-child(5) {
-			transition-delay: 0.6s;
-		}
-
-		& a {
-			display: flex;
-			align-items: center;
-			gap: 8px;
-			font-size: min(5.71vw, 16px);
-			color: ${(props) => props.theme.colors.txtNav};
-			font-weight: bold;
-
-			&::before {
-				content: "";
-				display: inline-block;
-				width: 16px;
-				height: 16px;
-				background-size: cover;
-				background-repeat: no-repeat;
-				background-position: center;
-			}
-
-			&.icon-about-us {
-				&::before {
-					background-image: url(/images/common/icon_about-us.png);
-				}
-			}
-			&.icon-project {
-				&::before {
-					background-image: url(/images/common/icon_project.png);
-				}
-			}
-			&.icon-blog {
-				&::before {
-					background-image: url(/images/common/icon_blog.png);
-				}
-			}
-			&.icon-donate {
-				&::before {
-					background-image: url(/images/common/icon_donate.png);
-				}
-			}
-		}
-	}
-
-	.open & li {
-		transform: translateY(0);
-		opacity: 1;
-	}
-`;
+import styles from "./NavList.module.scss";
 
 export default function NavList() {
+	const { isOpen } = useMenu();
 	const { pathname, isEnPage } = useLang();
 
 	return (
 		<nav>
-			<NavUl>
+			<ul className={isOpen ? `${styles.ul} ${styles.open}` : styles.ul}>
 				{nav.map((n, index) => {
 					return (
 						<li key={index}>
-							<Link href={isEnPage ? n.enLink : n.link} className={pathname === n.enLink ? `active ${n.iconCls}` : n.iconCls}>
+							<Link href={isEnPage ? n.enLink : n.link} className={styles[n.iconCls]}>
 								{isEnPage ? n.enText : n.text}
 							</Link>
 						</li>
 					);
 				})}
-			</NavUl>
+			</ul>
 		</nav>
 	);
 }
