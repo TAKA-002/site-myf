3e4d5babe6722a1e2d497f36d85f2a46b97eb8b8
miura takayuki <6015abc@gmail.com>
2025-03-18 20:43:48 +0900

    styleをCSS Modulesで分離

--- src/app/components/ui/overlay/MenuOverlay.tsx
+++ src/app/components/ui/overlay/MenuOverlay.tsx
@@ -1,30 +1,13 @@
+import styles from "./MenuOverlay.module.scss";
 import { ReactNode } from "react";
-import styled from "styled-components";
 import { useMenu } from "../../../context/MenuContext";
-
-const OverlayDiv = styled.div`
-	position: absolute;
-	inset: 0;
-	opacity: 0;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	background-color: ${(props) => props.theme.colors.overlay};
-	z-index: -1;
-	transition: opacity 0.5s;
-
-	&.open {
-		opacity: 0.8;
-		z-index: 10;
-	}
-`;
 
 export default function MenuOverlay({ children }: { children: ReactNode }) {
 	const { isOpen, closeMenu } = useMenu();
 
 	return (
-		<OverlayDiv className={isOpen ? "open" : ""} onClick={closeMenu}>
+		<div className={isOpen ? `${styles.div} ${styles.open}` : styles.div} onClick={closeMenu}>
 			{children}
-		</OverlayDiv>
+		</div>
 	);
 }
