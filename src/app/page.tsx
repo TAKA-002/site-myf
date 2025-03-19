97ca6ef169a5afb26bdf7394fbe6ce7f2185ec6b
miura takayuki <6015abc@gmail.com>
2025-03-19 19:32:53 +0900

    topページのコンポーネントを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
+import SectionHeadingLevel2nd from "./components/ui/heading/SectionHeadingLevel2nd";
+import Section from "./components/ui/layout/Section";
 import Swiper from "./components/ui/slider/swiper";
 
 export default function Home() {
 	return (
 		<>
-			<Swiper />
-			<h1>Welcome to my Next.js Site with TypeScript</h1>
-			<p>This is the homepage</p>
+			<Section name="slider">
+				<Swiper />
+			</Section>
+			<Section>
+				<SectionHeadingLevel2nd text="News" />
+			</Section>
 		</>
 	);
 }
