133dcf0ffe751a1e1b547f247687503067aaecc4
miura takayuki <6015abc@gmail.com>
2025-03-24 17:18:45 +0900

    noticeセクションのコーディング中

--- src/app/components/ui/list/Notice.tsx
+++ src/app/components/ui/list/Notice.tsx
@@ -1,19 +1,39 @@
 "use client";
 import Link from "next/link";
+import { ArrowRight } from "lucide-react";
 import { noticeList } from "@/constants/notice";
 import { useLang } from "@/context/LanguageContext";
+import styles from "@/components/ui/list/Notice.module.scss";
 
 export default function Notice() {
 	const { pathname, isEnPage } = useLang();
-	const href = () => {};
 
 	return (
-		<div className="">
+		<div className={styles.wrapper}>
 			{noticeList.map((n, index) => {
 				return (
-					<Link href={!isEnPage && n.link ? n.link : isEnPage && n.enLink ? n.enLink : ""} className="" key={index}>
-						<span>{n.date}</span>
-						<span>{isEnPage ? n.enText : n.text}</span>
+					<Link
+						href={
+							!isEnPage && n.link
+								? n.link
+								: isEnPage && n.enLink
+									? n.enLink
+									: ""
+						}
+						className={styles.anchor}
+						key={index}
+						aria-label={isEnPage ? n.enText : n.text}
+					>
+						<div>
+							<time className={styles.date}>{n.date}</time>
+							{n.tag && <span className={styles.tag}>{n.tag}</span>}
+						</div>
+						<div>
+							<span className={styles.text}>
+								{isEnPage ? n.enText : n.text}
+							</span>
+							<ArrowRight />
+						</div>
 					</Link>
 				);
 			})}
