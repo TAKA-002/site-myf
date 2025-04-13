e8b8a992d58195e04f377da417f108c057c2a2f5
miura takayuki <6015abc@gmail.com>
2025-04-13 17:10:01 +0900

    第2層のページレイアウトをコーディング実施

--- src/app/about-us/components/AboutUsContent.tsx
+++ src/app/about-us/components/AboutUsContent.tsx
@@ -1,6 +1,8 @@
 import Section from "@/components/ui/layout/Section";
 import VideoHeader from "@/components/ui/video/VideoHeader";
 import TimelineList from "@/components/ui/list/TimelineList";
+import { Card } from "@/components/ui/card/Cards";
+import { categoryData } from "@/constants/Cards";
 
 export default function AboutUsContent({ lang }: { lang: string }) {
 	return (
@@ -11,6 +13,10 @@ export default function AboutUsContent({ lang }: { lang: string }) {
 
 			<Section>
 				<TimelineList />
+			</Section>
+
+			<Section>
+				<Card data={categoryData} />
 			</Section>
 		</>
 	);
--- src/app/components/ui/list/TimelineList.tsx
+++ src/app/components/ui/list/TimelineList.tsx
@@ -1,8 +1,6 @@
 "use client";
 import Link from "next/link";
 import Image from "next/image";
-import HeadingLevel4 from "../heading/HeadingLevel4";
-import { Paragraph } from "../text/Paragraph";
 import styles from "@/components/ui/list/TimelineList.module.scss";
 import { Time } from "@/components/ui/text/Date";
 
@@ -11,14 +9,14 @@ const data = [
 		h2: "代表者メッセージ",
 		paragraph: "私はガーナの「PEACE SEED」の代表であり、慈善事業を行う慈善活動家であり、「MONPHY & YOUTH FOUNDATION（非営利団体）」の創設者であり代表です。",
 		date: 20250401,
-		img: "/images/about-us/tmb-message.jpg",
+		img: "/images/dmy.png",
 	},
 	{
 		h2: "なぜ活動をするのか。目標。",
 		paragraph:
 			"私は、自分のコミュニティの多くの人々に食事を提供し、支援してきたことで、その活動が認められてきました。学校の建設、学費の支払い、貧しい人々への経済的支援、病院代の支払い、いくつかの医療センターへの救急車の提供、人々への雇用の提供、さらには様々なコミュニティでの井戸掘りなどのプロジェクトへの資金提供など、彼らのニーズに応えてきました。",
 		date: 20250401,
-		img: "/images/about-us/tmb-message.jpg",
+		img: "/images/dmy.png",
 	},
 ];
 
@@ -39,7 +37,7 @@ export default function TimelineList() {
 								<h2>
 									<span>{d.h2}</span>
 								</h2>
-								<p>{d.paragraph}</p>
+								{d.paragraph && <p>{d.paragraph}</p>}
 								{d.date && <Time time={d.date} />}
 							</div>
 							<figure>
